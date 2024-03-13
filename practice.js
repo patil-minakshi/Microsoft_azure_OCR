caab3e1edd2599dafa50ad43cf304ea64cb34f93
Minakshi Patil <pminu1972000@gmail.com>
2024-03-13 18:57:51 +0530

    Read file synchrouncly

--- practice.js
+++ practice.js
@@ -1,35 +1,35 @@
 
-// const fs = require('fs')
+const fs = require('fs')
  
-// const path = require('path')
-// const DirectoryPath = "./PS1-TR-Data/groundtruth/" 
+const path = require('path')
+const DirectoryPath = "./PS1-TR-Data/groundtruth/" 
 
-// fs.readdir(DirectoryPath , function(err , files){
-//     if(err){
-//         console.log("error reading file" , err);
-//         return;
-//     }
+fs.readdir(DirectoryPath , function(err , files){
+    if(err){
+        console.log("error reading file" , err);
+        return;
+    }
 
-//     files.forEach((file)=>{
-//         const File = path.join(DirectoryPath , file)
+    files.forEach((file)=>{
+        const File = path.join(DirectoryPath , file)
 
-//         fs.readFile(File , 'utf-8' , function(err , data){
-//             if(err){
-//                 console.log("error reading text" , err);
-//             }
-//             console.log(data);
-//         } )
-//     })
-// })
+        fs.readFile(File , 'utf-8' , function(err , data){
+            if(err){
+                console.log("error reading text" , err);
+            }
+            console.log(data);
+        } )
+    })
+})
 
 
 
-// fs.readFile('./PS1-TR-Data/groundtruth/1.txt' , 'utf-8' ,  function(err , data){
-//     if(err){
-//         throw err;
-//     }
-//     const content = data
-//     console.log(content);
-// })
+fs.readFile('./PS1-TR-Data/groundtruth/1.txt' , 'utf-8' ,  function(err , data){
+    if(err){
+        throw err;
+    }
+    const content = data
+    console.log(content);
+})  // done dana done doneeeeeee
 
  
--- test.js
+++ test.js
@@ -1,23 +1,20 @@
  
-
-const fs = require('fs');
 const { ImageAnalysisClient , KeyCredential } = require ('@azure-rest/ai-vision-image-analysis');
 const createClient = require('@azure-rest/ai-vision-image-analysis').default;
 const { AzureKeyCredential } = require('@azure/core-auth');
-
-
  
+ 
+ 
+const fs = require('fs');
+ 
+const Destination = '././PS1-TR-Data/output.txt/'
+
 require('dotenv').config()
 
-const predicted =     'TR13X3008'
-
-const endpoint = process.env['VISION_ENDPOINT'] || '<your_endpoint>';
-const key = process.env['VISION_KEY'] || '<your_key>';
+const endpoint = process.env.VISION_ENDPOINT
+const key = process.env.VISION_KEY
 const credential = new AzureKeyCredential(key);
-
-
-
-const destinationFIle = './PS1-TR-Data/output.txt/'
+//const  allFiles = []
 
 const client = createClient (endpoint, credential);
 
@@ -33,13 +30,15 @@ const feature = [
 const ratio = [0.9, 1.33];
 
 const language = 'en'
-const imagePath = './PS1-TR-Data/images/27.jpg';
+const Folderpath = './PS1-TR-Data/images/';
+const GroundtruthPath = './PS1-TR-Data/groundtruth/'
 
-async function analyzeImageFromFile() {
-  const imageBuffer = fs.readFileSync(imagePath);
+async function analyzeImageFromFile(Imagepath) {
 
-  const result = await client.path('/imageanalysis:analyze').post({
-    body: imageBuffer,
+     try{
+        const Readed = fs.readFileSync(Imagepath)
+         const result = await client.path('/imageanalysis:analyze').post({
+    body: Readed,
     queryParameters: {
       features: feature,
       language: language,
@@ -47,42 +46,71 @@ async function analyzeImageFromFile() {
     },
     contentType: 'application/octet-stream'
   });
+  return result.body
+     }
+     catch (err){
+        console.log(err);
 
-  const iaResult = result.body;
-
-
-  if (iaResult.captionResult) {
-    console.log(`Caption: ${iaResult.captionResult.text} (confidence: ${iaResult.captionResult.confidence})`);
-  }
-  if (iaResult.readResult) {
-    iaResult.readResult.blocks.forEach(block => {
-      if(block.lines[0].text.split(' ').join('') != predicted){
-        const InCorrect = `${block.lines[0].text} , ${predicted } are not correct`
-        fs.appendFile(destinationFIle , InCorrect + '\n' , (err)=>{
-          if(err){
-            console.log(err);
-          }
-          else{
-            console.log("Non correct file send");
-          }
-          
-        }) 
-        // console.log(` ${block.lines[0].text} and ${predicted} are correct`);
-      } else{
-        const correct = `${block.lines[0].text} , ${predicted } are correct`
-        fs.appendFile(destinationFIle , correct  +  '\n' ,  (err) =>{
-          if(err){
-            console.log("correct writing error"  ,err);
-          }
-          else{
-            console.log(" correct file  send");
-          }
-        })
-      }
+     }
     
-    })    
 
-  }
 }
 
-analyzeImageFromFile();
+async function CompareString (){
+   
+ for (let i = 0; i< 100; i++) {
+  const Truthpath  = `${GroundtruthPath}${i}.txt`
+  const Imagepath = `${Folderpath}${i}.jpg`
+  const result = await analyzeImageFromFile(Imagepath)
+  if(result.readResult){
+    result.readResult.blocks.forEach(block => {
+        const Result = fs.readFileSync(Truthpath , 'utf-8').trim()
+        const TextfromImage = block.lines[0].text.replace(/\s/g, '')  // UTF12458
+        if(TextfromImage  != Result){
+
+            fs.appendFile(Destination , `${Result} and ${TextfromImage} are not same`  + '\n' ,(err)=>{
+                if(err){
+                    console.log("error appending file" ,err);
+                } else{
+                    console.log("apended wrong file");
+                }
+            })
+        } else{
+            fs.appendFile(Destination , `${Result} and ${TextfromImage} are same` + '\n'  ,(err) => {
+                if(err){
+                    console.log("error appending file" ,err);
+                } else{
+                    console.log("apended right file");
+                }
+            })
+        }
+
+    });
+  } else{
+    const massage = "cannot read this file"
+    fs.appendFile(Destination , massage + '\n' , (err)=>{
+        if(err){
+            console.log("error appending file" ,err);
+        }else{
+            console.log("done");
+        }
+    } )
+    console.log("no such a response found");
+  }
+//   const ImageResult = await analyzeImageFromFile(Imagepath)
+//   if(ImageResult.readResult){
+//     ImageResult.readResult.blocks.forEach(block => {
+         
+//         const truthBuffer = fs.readFileSync(Truthpath); // 0.txt
+//         const Stringtruth = truthBuffer.toString().trim()
+//         if(Stringtruth === `${block.lines[0].text}`){
+//             console.log(`${truthBuffer} ,${block.lines[0].text}`);
+//         }else{
+//             console.log("no file found");
+//         }
+//     })
+//   }
+}
+}
+CompareString()
+ 
